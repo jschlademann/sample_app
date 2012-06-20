d2ec067c73a772cf65ddbb9c1f2cb70962a95dcf
jschlademann <jschlademann@gmail.com>
2012-06-20 15:54:20 +0200

    user specs - first steps

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :name, :email
+	
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true
 end
